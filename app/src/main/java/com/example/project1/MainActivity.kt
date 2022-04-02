16cd1922274e9aa8ef9bf14bfc0953e8b00c34c5
neariahm <neariah.mandisa@gmail.com>
2022-04-02 17:35:45 -0400

    Updated main_activity.xml and MainActivity.kt and added 2 drawables

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.project1
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -7,17 +8,35 @@ import android.widget.ImageView
 import com.example.project1.ui.main.MainViewModel
 import androidx.core.net.toUri
 import coil.load
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 
 class MainActivity : AppCompatActivity() {
-    val viewModel : MainViewModel by viewModels()
+    val viewModel: MainViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    { super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        viewModel.currentlyDisplayedDogPhoto.observe(this,
-            { findViewById<ImageView>(R.id.imageView).load(
+
+        // Load Dog images into ImageView using Coil
+        viewModel.currentlyDisplayedDogPhoto.observe(this) {
+            findViewById<ImageView>(R.id.imageView).load(
                 it?.message?.toUri()?.buildUpon()?.scheme("https")?.build()
-            ) })
-        findViewById<Button>(R.id.button).setOnClickListener { viewModel.getRandomDogPhoto() } } }
+            )
+
+            // Additional Coil Features
+            {
+                crossfade(true)
+                crossfade(400)
+                placeholder(R.drawable.ic_pets)
+                error(R.drawable.ic_broken_image)
+                transformations(RoundedCornersTransformation(60f))
+            }
+        }
+
+        //Set a click listener on the Woof button to start network call
+        findViewById<Button>(R.id.button).setOnClickListener { viewModel.getRandomDogPhoto() }
+    }
+}
 
 
